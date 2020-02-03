973aa1a59da1c075d5e3c734b54d9ace6125b5a2
Peter Levart <tainn@protonmail.com>
2020-02-03 23:30:36 +0100

    Added the setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ookami',
+    version='1.0',
+    packages=['ookami'],
+    url='https://github.com/tainn1/ookami',
+    license='GNU GPLv3',
+    author='tainn',
+    author_email='tainn@protonmail.com',
+    description='Explicit discord webhook data manipulation'
+)
