79c5de6a963ce0c5e868da64a038dc6ebaf34f6c
Peter Levart <tainn@protonmail.com>
2020-08-18 14:45:45 +0200

    Version bump to 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ookami',
-    version='1.2',
+    version='1.3',
     packages=['ookami'],
     url='https://github.com/tainn/ookami',
     license='GNU GPLv3',
