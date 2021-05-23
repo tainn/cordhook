626413cb0ae3932b3b7beeba16b48c5f945567c7
tainn <tainn@protonmail.com>
2021-05-23 23:44:34 +0200

    Segments and standardizes the docstrings

--- ookami/ookami.py
+++ ookami/ookami.py
@@ -1,5 +1,3 @@
-# Ookami
-
 import os
 import json
 import requests
@@ -8,6 +6,7 @@ import requests
 class Ookami:
     """
     Allows for explicit creation, population and posting of a discord webhook
+    Segment 1 features form population methods, segment 2 features utility methods
     """
 
     def __init__(self) -> None:
@@ -20,42 +19,39 @@ class Ookami:
         with open(form_file, 'r') as jf:
             self.form: dict = json.load(jf)
 
-    def post(self, url: str = None) -> None:
-        """
-        Creates a post request on a provided webhook url
-        """
-        requests.post(url, json=self.form)
+    # Segment 1
 
-    def username(self, data: str = None) -> None:
+    def username(self, username: str = None) -> None:
         """
-        Overrides the current username of the webhook
+        :param username: overrides the current username of the webhook
         """
-        self.form['username'] = data
+        self.form['username'] = username
 
-    def avatar_url(self, data: str = None) -> None:
+    def avatar_url(self, avatar_url: str = None) -> None:
         """
-        Overrides the default avatar of the webhook
+        :param avatar_url: overrides the default avatar of the webhook
         """
-        self.form['avatar_url'] = data
+        self.form['avatar_url'] = avatar_url
 
-    def content(self, data: str = None) -> None:
+    def content(self, content: str = None) -> None:
         """
-        A simple message, contains up to 2000 characters
+        :param content: a simple message, contains up to 2000 characters
         """
-        self.form['content'] = data
+        self.form['content'] = content
 
-    def tts(self, data: bool = None) -> None:
+    def tts(self, tts: bool = None) -> None:
         """
-        If True, the message will be pronounced in the chat like a tts message
+        :param tts: if True, the message will be pronounced in the chat like a tts message
         """
-        self.form['tts'] = data
+        self.form['tts'] = tts
 
     def embeds_author(self, name: str = None, url: str = None, icon_url: str = None) -> None:
         """
-        Embed author object, includes:
-        - Name of the author
-        - Url of the author -- if name was used it becomes a hyperlink
-        - Url of the author icon
+        Embed author object
+
+        :param name: name of the author
+        :param url: url of the author; if name was used it becomes a hyperlink
+        :param icon_url: url of the author icon
         """
         self.form['embeds'][0]['author'] = {
             'name': name,
@@ -63,75 +59,86 @@ class Ookami:
             'icon_url': icon_url
         }
 
-    def embeds_color(self, data: int = None) -> None:
+    def embeds_color(self, color: int = None) -> None:
         """
-        Color code of the embed
-        Decimal numeral system is used, not hexadecimal
+        :param color: color code of the embed (decimal numeral system is used, not hexa)
         """
-        self.form['embeds'][0]['color'] = data
+        self.form['embeds'][0]['color'] = color
 
-    def embeds_title(self, data: str = None) -> None:
+    def embeds_title(self, title: str = None) -> None:
         """
-        Title of the embed
+        :param title: title of the embed
         """
-        self.form['embeds'][0]['title'] = data
+        self.form['embeds'][0]['title'] = title
 
-    def embeds_url(self, data: str = None) -> None:
+    def embeds_url(self, url: str = None) -> None:
         """
-        Url of the embed -- if title was used, it becomes a hyperlink
+        :param url: url of the embed; if title was used, it becomes a hyperlink
         """
-        self.form['embeds'][0]['url'] = data
+        self.form['embeds'][0]['url'] = url
 
-    def embeds_description(self, data: str = None) -> None:
+    def embeds_description(self, description: str = None) -> None:
         """
-        Description text
+        :param description: description text
         """
-        self.form['embeds'][0]['description'] = data
+        self.form['embeds'][0]['description'] = description
 
     def embeds_fields(self, name: str = None, value: str = None, inline: bool = None) -> None:
         """
-        Array of embed field objects, each element includes:
-        - Name of the field
-        - Value of the field
-        - Inline option -- if True, fields will be displayed in the same line
+        Array of embed field objects
+
+        :param name: name of the field
+        :param value: value of the field
+        :param inline: inline option; if True, fields will be displayed in the same line
         """
         self.form['embeds'][0]['fields'].append(
-            {'name': name,
-             'value': value,
-             'inline': inline}
+            {
+                'name': name,
+                'value': value,
+                'inline': inline
+            }
         )
-
-    def embeds_fields_count(self) -> int:
-        """
-        Returns the amount of elements in the embed fields list
-        """
-        return len(self.form['embeds'][0]['fields'])
 
     def embeds_thumbnail(self, url: str = None) -> None:
         """
-        Embed thumbnail object
+        :param url: embed thumbnail object
         """
         self.form['embeds'][0]['thumbnail'] = {'url': url}
 
     def embeds_image(self, url: str = None) -> None:
         """
-        Embed image object, includes an image url
+        :param url: embed image object, includes an image url
         """
         self.form['embeds'][0]['image'] = {'url': url}
 
     def embeds_footer(self, text: str = None, icon_url: str = None) -> None:
         """
-        Embed footer object, includes:
-        - Footer text -- does not support Markdown
-        - Url of the footer icon
+        Embed footer object
+
+        :param text: footer text; does not support Markdown
+        :param icon_url: url of the footer icon
         """
         self.form['embeds'][0]['footer'] = {
             'text': text,
             'icon_url': icon_url
         }
 
-    def embeds_timestamp(self, data: str = None) -> None:
+    def embeds_timestamp(self, timestamp: str = None) -> None:
         """
-        ISO8601 timestamp (yyyy-mm-ddThh:mm:ss.msZ)
+        :param timestamp: ISO8601 timestamp (yyyy-mm-ddThh:mm:ss.msZ)
         """
-        self.form['embeds'][0]['timestamp'] = data
+        self.form['embeds'][0]['timestamp'] = timestamp
+
+    # Segment 2
+
+    def post(self, url: str = None) -> None:
+        """
+        :param url: creates a post request on a provided webhook url
+        """
+        requests.post(url, json=self.form)
+
+    def embeds_fields_count(self) -> int:
+        """
+        :return: the amount of elements in the embed fields list
+        """
+        return len(self.form['embeds'][0]['fields'])
